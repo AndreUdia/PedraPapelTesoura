d0f8685303c99f9a4182d5d0d260b3c962304745
cabomaldade <soutom@yahoo.com.br>
2018-09-23 14:51:03 -0300

    Criando a escolha do app

--- app/src/main/java/com/example/andre/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/andre/pedrapapeltesoura/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.andre.pedrapapeltesoura;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,38 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionarPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+    public void selecionarTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String escolhaUsuario){
+
+        ImageView imagemResultado = (ImageView) findViewById(R.id.imageResultado);
+
+        // gerando número aleatório para escolha do App
+        String [] opcoes = {"pedra", "papel", "tesoura"};
+        int numero = new Random().nextInt(3);
+        String escolhaApp = opcoes[numero];
+
+        switch ( escolhaApp ){
+            case "pedra" :
+                imagemResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel" :
+                imagemResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura" :
+                imagemResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
     }
 }
